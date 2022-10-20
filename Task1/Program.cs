944f030d892f98278ac81617b6a5e29bc0aebced
Yuliya <8avokado8@gmail.com>
2022-10-21 01:18:52 +0600

    Done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,18 +3,26 @@
 //456 -> 5
 //782 -> 8
 //918 -> 1
+
+
 Console.WriteLine("Введите трехзначное число");
 
 int a = Convert.ToInt32(Console.ReadLine());
 
-
-if (99 < a && a < 1000) ;
+if (99 > a || a > 1000)
+{
+    do
+    {
+        Console.WriteLine("Ошибка! Введите трехзначное число");
+        a = Convert.ToInt32(Console.ReadLine());
+    }
+    while (99 > a || a > 1000);
+}
+if (99 < a && a < 1000)
 {
     int b = a / 10;
     int c = b % 10;
     Console.WriteLine(c);
 }
-//else
-//{
-//Console.WriteLine("Ошибка! Введите трехзначное число"); //	"message": "\"else\" не может запускать оператор. [OmniSharpMiscellaneousFiles.csproj]",
-//}
+
+
